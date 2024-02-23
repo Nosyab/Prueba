0b70c37cadc93db6da66a1a428f408260a62216c
Jair Beltrán <jairabs.b.f@gmail.com>
2024-02-23 13:44:59 -0300

    modificación de js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
     $(this).hide("slow");
     });
     });
-
+    
+//efecto al pasarl el mouse por viajes Chile
     $(document).ready(function(){
     $("h4").mouseover (function () {
         $(this).hide();
     });
     });
 
+// alerta de inicio de página
    alert("Bienvenidos a Viajes Chile")
 
+// alerta de boton de envio de formulario 
    document.getElementById('cambiar-color').onclick = function(){
     alert('Formulario fue Enviado!');
 }
