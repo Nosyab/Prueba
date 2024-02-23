57c3af2e3025d45596f61eea0bb2ce691fb58c7f
Jair Beltrán <jairabs.b.f@gmail.com>
2024-02-23 10:49:37 -0300

    Primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("p").dblclick(function(){
+    $(this).hide("slow");
+    });
+    });
+
+    $(document).ready(function(){
+    $("h4").mouseover (function () {
+        $(this).hide();
+    });
+    });
+
+   alert("Bienvenidos a Viajes Chile")
+
+   document.getElementById('cambiar-color').onclick = function(){
+    alert('Formulario fue Enviado!');
+}
